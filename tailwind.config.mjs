d25e8874fbc18ac98610f3f23712e6ff4424992d
David Arenas <davejs136@gmail.com>
2024-03-04 00:50:21 -0400

    feat: add secondContactSection

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,6 @@ export default {
       fontFamily: {
         sans: ["Noto Sans JP Variable", "sans-serif"],
       },
-      backgroundImage: {
-        "radial-gradient":
-          "background: radial-gradient(50% 50% at 50% 50%, #FEF3A4 0%, #BA891F 100%)",
-      },
     },
   },
   plugins: [],
